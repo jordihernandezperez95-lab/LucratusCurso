21f35630251233d284181391dba47d5323e7ecce
Ditarex <Ditarex95@gmail.com>
2025-11-26 19:25:35 +0100

    Correccion del ejercicio de Alicia

---
+++ PYTHON/clase1.py
@@ -0,0 +1,41 @@
+# Comentario de una línea
+
+"""
+Esto es un comentario multilinea
+puedes hacer varias
+"""
+
+# print("Hola mundo!")
+
+# type 
+print( type("Hola mundo") )
+print( type(58) )
+print( type(0.755) )
+print( type(False) )
+
+# Como crear una variable y usarla
+texto = "Esto es un string/texto"
+print(texto)
+texto = 5
+print(texto)
+texto = False
+
+print(f'{texto} que es de tipo {type(texto)}')
+
+numero1 = 5
+numero2 = 2
+texto = "String 1" + " String 2"
+
+# Operadores aritmeticos
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+producto = numero1 * numero2
+division = numero1 / numero2
+potencia = numero1 ** numero2
+
+print(f'{numero1} + {numero2} = {suma}' )
+
+print(texto)
+
+texto
